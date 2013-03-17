8fb2a0bca40443a4963c517a5eb8646d41c6caf6
Gordon Marler <gmarler@gmarler.com>
2013-03-17 00:44:35 -0400

    add TODO list

--- lib/Solaris/StatParse.pm
+++ lib/Solaris/StatParse.pm
@@ -2,5 +2,10 @@ use strict;
 use warnings;
 
 package Solaris::StatParse;
+# ABSTRACT: A Parser for Solaris Statistics programs
+# VERSION
+use Moose;
+
+
 
 1;
---
+++ t/01parseobj.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+
+use Test::Most tests => 1;                      # last test to print
+
+
+
