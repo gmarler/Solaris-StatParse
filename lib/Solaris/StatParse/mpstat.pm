1208796342bcd90414d551378cf7f441b75d1d0c
Gordon Marler <gmarler@gmarler.com>
2013-03-23 23:40:49 -0400

    add parse_time method
    Update date_regex handling
    add test to ensure correct number of dates detected by date_regex

--- lib/Solaris/StatParse/mpstat.pm
+++ lib/Solaris/StatParse/mpstat.pm
@@ -4,12 +4,36 @@ use warnings;
 package Solaris::StatParse::mpstat;
 
 use Moose;
+use IO::File;
 
 has 'file'       => ( is => 'rw',
                       isa => 'Str', );
 has 'date_regex' => ( is => 'ro',
-                      isa => 'RegexRef',
+                      isa => 'RegexpRef',
+                      default =>
+                        sub {
+                          qr{^ Mon|Tue|Wed|Thu|Fri|Sat|Sun \s+
+                            }x;
+                        },
                     );
+
+
+sub parse_time {
+  my $self = shift;
+
+  my $count;
+  my $file = $self->file;
+  my $fh   = IO::File->new($file, "<") or die "Unable to open $file: $!";
+  my $re   = $self->date_regex;
+
+  while (my $line = <$fh>) {
+    if ($line =~ m/$re/smx) {
+      $count++;
+    }
+  }
+  $fh->close;
+  return $count;
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
--- t/02dateparse.t
+++ t/02dateparse.t
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 4;                      # last test to print
+use Test::More tests => 5;                      # last test to print
 use FindBin;
 
 
@@ -15,3 +15,9 @@ my $file1 = "$FindBin::Bin/data/mpstat.1";
 $mpstat = Solaris::StatParse::mpstat->new( file => $file1 );
 isa_ok( $mpstat, "Solaris::StatParse::mpstat");
 ok( $mpstat->file() eq $file1, "Stored file is $file1" );
+
+#warn $mpstat->dump;
+
+# TODO: Take above and put into a test for the file attribute
+#
+ok( $mpstat->parse_time() == 10, "Found 10 times" );
